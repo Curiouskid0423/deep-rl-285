d0d3e8251985154d036c6374c40c7e8c0f444859
Curiouskid0423 <yutengli@berkeley.edu>
2022-09-12 07:10:44 +0000

    moving experiment to facelab-research clusters.

--- hw1/cs285/scripts/run_DAgger.sh
+++ hw1/cs285/scripts/run_DAgger.sh
@@ -4,7 +4,7 @@ python cs285/scripts/run_hw1.py \
     --expert_data cs285/expert_data/expert_data_Ant-v4.pkl \
     --exp_name dagger_ant \
     --n_iter 10 \
-    --num_agent_train_steps_per_iter 2000 \
+    --num_agent_train_steps_per_iter 4000 \
     --do_dagger \
     --ep_len 1000 \
     --eval_batch_size 10000 \
